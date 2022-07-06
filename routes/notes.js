f00286ac84bdd4910bfe62649fd3da72f20d3aa1
kate patch <kate.epatch@gmail.com>
2022-07-06 16:49:51 -0600

    changed up the notes.js since it wasn't working

--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,57 @@
-const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+// const notes = require('express').Router();
+// const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+// const uuid = require('../helpers/uuid');
+
+// notes.get('/api/notes', (req, res) => {
+//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// });
+
+// notes.post('/spi/notes', (req, res) => {
+//     console.log(req.body);
+
+//     const { noteTitle, noteText } = req.body;
+
+//     if (req.body) {
+//         const newNote = {
+//             noteTitle,
+//             noteText,
+//         };
+
+//         readAndAppend(newNote, './db/db.json');
+//         res.json(`Note Added`);
+//     } else {
+//         res.error(`Could not add note`);
+//     }
+// });
+
+// module.exports = notes;
+
+const path = require('path');
+const fs = require('fs');
 const uuid = require('../helpers/uuid');
+const { append } = require('vary');
 
-notes.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+
+//GET api/notes
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
-notes.post('/spi/notes', (req, res) => {
-    console.log(req.body);
+//POST api/notes
+app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
 
-    const { noteTitle, noteText } = req.body;
-
-    if (req.body) {
-        const newNote = {
-            noteTitle,
-            noteText,
-        };
-
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note Added`);
-    } else {
-        res.error(`Could not add note`);
-    }
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid(),
+    };
+    db.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
 });
 
-module.exports = notes;
+//DELETE api/notes
+
+module.exports = app;
--- server.js
+++ server.js
@@ -27,3 +27,4 @@ app.get('*', (req, res) =>
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} :rocket:`)
 );
+
