908d2ac35cd09cfa16bb1c8cbabef5ccf45998b1
kate patch <kate.epatch@gmail.com>
2022-07-06 16:39:06 -0600

    finished notes.js formulas hope they work

--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,26 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-notes.get('/', (req, res) => {
-    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+notes.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.post('/', (req, res) => {
+notes.post('/spi/notes', (req, res) => {
     console.log(req.body);
 
-    
-})
+    const { noteTitle, noteText } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            noteTitle,
+            noteText,
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note Added`);
+    } else {
+        res.error(`Could not add note`);
+    }
+});
+
+module.exports = notes;
