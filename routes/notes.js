027328200e2856cd42cff239806abe16745e9cbe
kate patch <kate.epatch@gmail.com>
2022-07-06 15:52:04 -0600

    added server.js file

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,7 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../')
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+notes.get('/', (req, res) => {
+    readFromFile()
+})
