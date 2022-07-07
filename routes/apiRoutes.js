cb78b473940542ca02ab4833684e1632128a97b5
kate patch <kate.epatch@gmail.com>
2022-07-06 18:50:02 -0600

    changed up the GET route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,12 +30,19 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('../helpers/uuid');
 const { append } = require('vary');
+const notes = require('express').Router();
+
+// GET Route for retrieving all the notes = /api/notes/
+notes.get('/', (req, res) => {
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
 
 
 //GET api/notes
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../db/db.json'));
+// });
 
 //POST api/notes
 app.post('/api/notes', (req, res) => {
@@ -55,3 +62,5 @@ app.post('/api/notes', (req, res) => {
 //DELETE api/notes
 
 module.exports = app;
+
+
