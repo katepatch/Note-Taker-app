733b66b631169f49b2c0a08daf5007564ccec55b
kate patch <kate.epatch@gmail.com>
2022-07-07 13:02:09 -0600

    created a notes file cause im gonna try something

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,8 @@
-// const notes = require('express').Router();
-// const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-// const uuid = require('../helpers/uuid');
-
-// notes.get('/api/notes', (req, res) => {
-//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-// });
-
-// notes.post('/spi/notes', (req, res) => {
-//     console.log(req.body);
-
-//     const { noteTitle, noteText } = req.body;
-
-//     if (req.body) {
-//         const newNote = {
-//             noteTitle,
-//             noteText,
-//         };
-
-//         readAndAppend(newNote, './db/db.json');
-//         res.json(`Note Added`);
-//     } else {
-//         res.error(`Could not add note`);
-//     }
-// });
-
-// module.exports = notes;
-
 const path = require('path');
 const fs = require('fs');
 const uuid = require('../helpers/uuid');
 //const { append } = require('vary');
-const notes = require('express').Router();
+const app = require('express').Router();
 
 // GET Route for retrieving all the notes = /api/notes/
 app.get('/', (req, res) => {
