72f0a0a2e90020c67e3566a4e0e1f4db4f6e7b4c
kate patch <kate.epatch@gmail.com>
2022-07-07 15:02:30 -0600

    trying to return files

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-// // GET Route for feedback page
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
-// );
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
