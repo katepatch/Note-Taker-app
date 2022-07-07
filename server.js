23785eecbed486729a3e0a242eeca4fab4db7852
kate patch <kate.epatch@gmail.com>
2022-07-06 19:30:01 -0600

    fixing server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/htmlRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.');
+const apiRoutes = require('.routes/apiRoutes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
-);
+// // GET Route for feedback page
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+// );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
