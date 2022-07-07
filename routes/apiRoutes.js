14d0c84bdd5ab87631885cf62f8dafa13bf782c5
kate patch <kate.epatch@gmail.com>
2022-07-07 14:47:53 -0600

    able to open up local host but error says no such file or directory when clicking link to add new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,29 +10,11 @@ app.get('/notes', (req, res) => {
     res.json(input);
 });;
 
-
-
-//GET api/notes
-// app.get('/api/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../db/db.json'));
-// });
-
 //POST api/notes
 app.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     const userNote = newNote(req.body, notes);
     res.json(userNote);
-    // let db = fs.readFileSync('db/db.json');
-    // db = JSON.parse(db);
-    // res.json(db);
-
-    // let newNote = {
-    //     title: req.body.title,
-    //     text: req.body.text,
-    //     id: uuid(),
-    // };
-    // db.push(newNote);
-    // fs.writeFileSync('db/db.json', JSON.stringify(db));
 });
 
 //DELETE api/notes
