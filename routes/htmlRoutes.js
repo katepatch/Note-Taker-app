1ac50e034824879e47214206b5d4004c9c5b6430
kate patch <kate.epatch@gmail.com>
2022-07-07 13:31:54 -0600

    hopefully fixed things in the htmlRouter file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,22 @@
 const path = require('path');
-
+const app = require('express').Router();
 
 
 module.exports = (app) => {
 //GET /notes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
-}
+};
 
 
