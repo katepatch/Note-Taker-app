580301c2e1a428a3b0488bc909a267b55f229ba7
kate patch <kate.epatch@gmail.com>
2022-07-07 13:38:09 -0600

    updated a few typos.  something about can't find module uuid

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes.');
-const apiRoutes = require('.routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
