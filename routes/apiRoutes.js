100bb256610efaaa656b1f2218c3fc6dcbe2d874
kate patch <kate.epatch@gmail.com>
2022-07-07 14:50:01 -0600

    changing apiroutes variable names

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,28 @@
 const {notes} = require('../db/db.json')
 const { v4: uuidv4 } = require('uuid');
 const { newNote, newDb } = require('../lib/notes');
-const app = require('express').Router();
+const router = require('express').Router();
 
 // GET Route for retrieving all the notes = /api/notes/
-app.get('/notes', (req, res) => {
-//   readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+router.get('/notes', (req, res) => {
     let input = notes;
     res.json(input);
 });;
 
 //POST api/notes
-app.post('/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     const userNote = newNote(req.body, notes);
     res.json(userNote);
 });
 
 //DELETE api/notes
-app.delete("/notes/:id", (req, res) => {
+router.delete("/notes/:id", (req, res) => {
     const params = req.params.id
     newDb(params, notes);
     res.redirect('');
 });
 
-module.exports = app;
+module.exports = router;
 
 
